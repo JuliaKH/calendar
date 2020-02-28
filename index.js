606be350a4ef18ae83fe0788d75e5d5e19d2dad9
Julia <sk8ergirl1007@gmail.com>
2020-02-28 11:01:59 +0200

    new commit

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 var months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
 var date = new Date();
-var month = date.getMonth() + 1;
+// var month = date.getMonth() + 1;
 var year = date.getFullYear();
 
 
 $(function () {
+    fillMonthName();
+    fillMonthDays();
+});
+
+function fillMonthName() {
     $(".month-name").each(function (index) {
         $(this).text(months[index]);
     });
-
+}
+function fillMonthDays() {
     $(".month-days").each(function (index) {
         $(this).text(new Date(year, index+1, 0).getDate());
+        $(this).append("<tr></tr>");
     });
-});
+}
+
+console.log('Revert me');
+console.log('Revert me');
